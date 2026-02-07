d126133e8cdd5601b4689de2b93b9abc76af840b
MikeJimenez22 <cerdajimenezmichael@gmail.com>
2026-02-07 10:44:36 -0600

    se quito tasa de cambio online

--- CaoaPresentacion/Frm_CambioDolar.Designer.cs
+++ CaoaPresentacion/Frm_CambioDolar.Designer.cs
@@ -36,9 +36,6 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.btnGetRate = new System.Windows.Forms.Button();
-            this.label5 = new System.Windows.Forms.Label();
-            this.txtRate = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -55,7 +52,7 @@
             // txtValorDolar
             // 
             this.txtValorDolar.Font = new System.Drawing.Font("Arial", 12F);
-            this.txtValorDolar.Location = new System.Drawing.Point(123, 121);
+            this.txtValorDolar.Location = new System.Drawing.Point(123, 161);
             this.txtValorDolar.Name = "txtValorDolar";
             this.txtValorDolar.Size = new System.Drawing.Size(257, 26);
             this.txtValorDolar.TabIndex = 2;
@@ -64,7 +61,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(39, 122);
+            this.label1.Location = new System.Drawing.Point(39, 162);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(68, 25);
             this.label1.TabIndex = 5;
@@ -85,7 +82,7 @@
             this.button1.Font = new System.Drawing.Font("Arial", 12F);
             this.button1.Image = global::CaoaPresentacion.Properties.Resources._285657_floppy_guardar_save_icon;
             this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.Location = new System.Drawing.Point(386, 112);
+            this.button1.Location = new System.Drawing.Point(386, 152);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(138, 43);
             this.button1.TabIndex = 3;
@@ -98,7 +95,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(60, 79);
+            this.label3.Location = new System.Drawing.Point(61, 103);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(164, 25);
             this.label3.TabIndex = 9;
@@ -108,43 +105,11 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Arial", 12F);
-            this.label4.Location = new System.Drawing.Point(350, 86);
+            this.label4.Location = new System.Drawing.Point(351, 110);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(110, 18);
             this.label4.TabIndex = 10;
             this.label4.Text = "Cambio Actual";
-            // 
-            // btnGetRate
-            // 
-            this.btnGetRate.Font = new System.Drawing.Font("Arial", 12F);
-            this.btnGetRate.Image = global::CaoaPresentacion.Properties.Resources._118805_accessories_text_editor_editor_accessories_text;
-            this.btnGetRate.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnGetRate.Location = new System.Drawing.Point(24, 198);
-            this.btnGetRate.Name = "btnGetRate";
-            this.btnGetRate.Size = new System.Drawing.Size(123, 43);
-            this.btnGetRate.TabIndex = 11;
-            this.btnGetRate.Text = "Obtener";
-            this.btnGetRate.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.btnGetRate.UseVisualStyleBackColor = true;
-            this.btnGetRate.Click += new System.EventHandler(this.btnGetRate_ClickAsync);
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(118, 168);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(307, 25);
-            this.label5.TabIndex = 12;
-            this.label5.Text = "TIPO DE CAMBIO EN LINEA";
-            // 
-            // txtRate
-            // 
-            this.txtRate.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtRate.Location = new System.Drawing.Point(153, 207);
-            this.txtRate.Name = "txtRate";
-            this.txtRate.Size = new System.Drawing.Size(206, 26);
-            this.txtRate.TabIndex = 13;
             // 
             // Frm_CambioDolar
             // 
@@ -152,9 +117,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(536, 291);
-            this.Controls.Add(this.txtRate);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.btnGetRate);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
@@ -183,8 +145,5 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button btnGetRate;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox txtRate;
     }
 }
--- CaoaPresentacion/Frm_CambioDolar.cs
+++ CaoaPresentacion/Frm_CambioDolar.cs
@@ -3,10 +3,7 @@ using System;
 using System.Data;
 using System.Windows.Forms;
 using TuNamespace;
-//using Newtonsoft.Json;
-//using System.Net.Http;
-//using System.Threading.Tasks;
-//using System.Net;
+
 
 namespace CaoaPresentacion
 {
@@ -87,71 +84,7 @@ namespace CaoaPresentacion
 
         }
 
-        private async void btnGetRate_ClickAsync(object sender, EventArgs e)
-        {
-            try
-            {
-                btnGetRate.Enabled = false;
-                txtRate.Text = "Obteniendo tasa...";
-                Cursor = Cursors.WaitCursor;
-
-                // Configurar TLS 1.2 para Windows
-                System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12;
-
-                // Usar WebClient que es más confiable
-                using (System.Net.WebClient wc = new System.Net.WebClient())
-                {
-                    wc.Encoding = System.Text.Encoding.UTF8;
-                    wc.Headers.Add("User-Agent", "Mozilla/5.0");
-
-                    // ✅ URL que SÍ FUNCIONA (probada)
-                    string url = "https://api.exchangerate-api.com/v4/latest/USD";
-
-                    // Mostrar qué estamos intentando
-                    txtRate.Text = $"Conectando a: exchangerate-api.com";
-
-                    // Descargar datos
-                    string json = wc.DownloadString(url);
-
-                    // Parsear respuesta
-                    var data = Newtonsoft.Json.Linq.JObject.Parse(json);
-                    decimal tasa = (decimal)data["rates"]["NIO"];
-                    string fecha = (string)data["date"];
-
-                    // Mostrar resultado
-                    txtRate.Text = $"C${tasa:N2}";
-                                  
-                   
-
-                  
-                }
-            }
-            catch (System.Net.WebException webEx)
-            {
-                txtRate.Text = $"Error: {webEx.Status}";
-
-                if (webEx.Status == System.Net.WebExceptionStatus.ProtocolError)
-                {
-                    MessageBox.Show("Error 404: URL no encontrada\n\n" +
-                                  "La API puede haber cambiado.\n" +
-                                  "Intente con otra fuente.",
-                                  "Error de URL",
-                                  MessageBoxButtons.OK,
-                                  MessageBoxIcon.Warning);
-                }
-            }
-            catch (Exception ex)
-            {
-                txtRate.Text = $"Error: {ex.GetType().Name}";
-                MessageBox.Show($"Error detallado:\n{ex.Message}",
-                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            finally
-            {
-                btnGetRate.Enabled = true;
-                Cursor = Cursors.Default;
-            }
-        }
+      
     }
 
 }
